a139c933d69688a5fcc62e78d92160902c92a1fe
Marco Bungart <marco.bungart@consol.de>
2020-05-14 09:28:30 +0200

    Added endpoint to create new users

--- src/main/java/de/consol/dus/boundary/endpoints/UserResource.java
+++ src/main/java/de/consol/dus/boundary/endpoints/UserResource.java
@@ -1,19 +1,32 @@
 package de.consol.dus.boundary.endpoints;
 
+import de.consol.dus.boundary.entities.UserEntity;
+import de.consol.dus.boundary.mapper.UserMapper;
+import de.consol.dus.boundary.repositories.UserRepository;
+import de.consol.dus.boundary.requests.CreateUserRequest;
 import de.consol.dus.boundary.responses.UserResponse;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import lombok.AllArgsConstructor;
 
-@Path("/hello")
+@AllArgsConstructor
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     public static final String DEFAULT_EMAIL = "john@Doe.com";
 
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public UserResponse getSampleUser() {
         return UserResponse.builder()
             .name("John Doe")
@@ -23,11 +36,18 @@ public class UserResource {
 
     @GET
     @Path("{name}")
-    @Produces(MediaType.APPLICATION_JSON)
     public UserResponse getUserByName(@PathParam("name") String name) {
         return UserResponse.builder()
             .name(name)
             .email(DEFAULT_EMAIL)
             .build();
+    }
+
+    @POST
+    @Transactional
+    public UserResponse createUser(CreateUserRequest request) {
+        UserEntity toSave = mapper.requestToEntity(request);
+        repository.persist(toSave);
+        return mapper.entityToResponse(toSave);
     }
 }
