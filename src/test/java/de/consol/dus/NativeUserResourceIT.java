e0b1e6c6938646bf9a77695371fa2b9c496e4e61
Marco Bungart <marco.bungart@consol.de>
2020-05-14 08:27:56 +0200

    Initial commit

---
+++ src/test/java/de/consol/dus/NativeUserResourceIT.java
@@ -0,0 +1,9 @@
+package de.consol.dus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeUserResourceIT extends UserResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/de/consol/dus/UserResourceTest.java
@@ -0,0 +1,21 @@
+package de.consol.dus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class UserResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
