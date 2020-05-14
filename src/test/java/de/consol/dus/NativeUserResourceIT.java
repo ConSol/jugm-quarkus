48808f2e3b259df5a28ec8d30e17bf02b4dd0974
Marco Bungart <marco.bungart@consol.de>
2020-05-14 09:28:37 +0200

    Removed sample tests

--- src/test/java/de/consol/dus/NativeUserResourceIT.java
+++
@@ -1,9 +0,0 @@
-package de.consol.dus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeUserResourceIT extends UserResourceTest {
-
-    // Execute the same tests but in native mode.
-}
