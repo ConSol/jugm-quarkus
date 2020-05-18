9f565878d502965b4173ce5007173de70af856d6
Marco Bungart <marco.bungart@consol.de>
2020-05-18 13:36:53 +0200

    Replaced fallback with circuit breaker.
    Notice that the status of the circuit breaker is shown in the ready check.

--- src/main/java/de/consol/dus/boundary/restclients/FruitRestClient.java
+++ src/main/java/de/consol/dus/boundary/restclients/FruitRestClient.java
@@ -1,16 +1,13 @@
 package de.consol.dus.boundary.restclients;
 
 import de.consol.dus.boundary.responses.FruitResponse;
-import de.consol.dus.boundary.responses.NutritionResponse;
 import java.time.temporal.ChronoUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.eclipse.microprofile.faulttolerance.ExecutionContext;
-import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.FallbackHandler;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -22,22 +19,9 @@ public interface FruitRestClient {
   @GET
   @Path("{name}")
   @Retry(maxRetries = 5, delay = 1, delayUnit = ChronoUnit.SECONDS)
-  @Fallback(FruitFallback.class)
+  @CircuitBreaker(
+      requestVolumeThreshold = 4,
+      delay = 30,
+      delayUnit = ChronoUnit.SECONDS)
   FruitResponse getFruitByName(@PathParam("name") String name);
-
-  final class FruitFallback implements FallbackHandler<FruitResponse> {
-
-    @Override
-    public FruitResponse handle(ExecutionContext context) {
-      return FruitResponse.builder()
-          .name("Fallback Apple")
-          .nutritions(NutritionResponse.builder()
-              .calories(133.7)
-              .carbohydrates(66.6)
-              .fat(42.0)
-              .build())
-          .comment("This is good & evil")
-          .build();
-    }
-  }
 }
