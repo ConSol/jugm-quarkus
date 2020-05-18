7bf2b2aa097c8df4dc911405792b0dc609846b71
Marco Bungart <marco.bungart@consol.de>
2020-05-18 13:25:04 +0200

    Added fallback.
    Notice that the ready check now succeeds.

--- src/main/java/de/consol/dus/boundary/responses/FruitResponse.java
+++ src/main/java/de/consol/dus/boundary/responses/FruitResponse.java
@@ -3,7 +3,9 @@ package de.consol.dus.boundary.responses;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@SuperBuilder
 @NoArgsConstructor
 @Getter
 @Setter
--- src/main/java/de/consol/dus/boundary/restclients/FruitRestClient.java
+++ src/main/java/de/consol/dus/boundary/restclients/FruitRestClient.java
@@ -1,12 +1,16 @@
 package de.consol.dus.boundary.restclients;
 
 import de.consol.dus.boundary.responses.FruitResponse;
+import de.consol.dus.boundary.responses.NutritionResponse;
 import java.time.temporal.ChronoUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -18,5 +22,22 @@ public interface FruitRestClient {
   @GET
   @Path("{name}")
   @Retry(maxRetries = 5, delay = 1, delayUnit = ChronoUnit.SECONDS)
+  @Fallback(FruitFallback.class)
   FruitResponse getFruitByName(@PathParam("name") String name);
+
+  final class FruitFallback implements FallbackHandler<FruitResponse> {
+
+    @Override
+    public FruitResponse handle(ExecutionContext context) {
+      return FruitResponse.builder()
+          .name("Fallback Apple")
+          .nutritions(NutritionResponse.builder()
+              .calories(133.7)
+              .carbohydrates(66.6)
+              .fat(42.0)
+              .build())
+          .comment("This is good & evil")
+          .build();
+    }
+  }
 }
